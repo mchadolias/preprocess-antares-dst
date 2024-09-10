a4d5612160bc187afa9f8cc034c087064acc9362
Michael Chadolias <mchadolias@km3net.de>
2024-09-10 15:59:06 +0200

    Update muon job script

--- add_NNFit/job_add_branch_muons.sh
+++ add_NNFit/job_add_branch_muons.sh
@@ -11,38 +11,37 @@
 ### SLURM
 
 #SBATCH --ntasks=1                    # Run a single task (by default tasks == CPU)
-#SBATCH --mem=200G                      # GB
-#SBATCH --time=01-00:00:00               #
+#SBATCH --mem=180G                      # GB
+#SBATCH --time=00-02:30:00               #
 #SBATCH --mail-user=mchadolias@km3net.de   # Where to send mail
 #SBATCH --mail-type=FAIL,TIME_LIMIT              # Mail events (NONE, BEGIN, END, FAIL, ALL)
 
-echo "Running add_column_test.py"
+echo "Running add_column_branch.py"
 echo "ROOT_FILE: ${ROOT_FILE}"
 echo "H5_FILE: ${H5_FILE}"
 echo "TASK: ${TASK}"
 
-# Define the name of the Anaconda environment to activate
-ENVNAME="km3net-bootcamp"
-
-# Define the path to the Anaconda installation's 'bin' directory2
-ANACONDAPATH="/home/saturn/capn/mppi133h/software/private/conda/envs/km3net-bootcamp/bin"
-WORKDIR="/home/saturn/capn/mppi133h/master_thesis/antares_dst/add_NNFit"
-
-# Add the Anaconda installation's 'bin' directory to the PATH variable
-export PATH="$ANACONDAPATH:$PATH"
-
-# Activate the conda environment
-which python
+WORKDIR="$WORK/master_thesis/antares_dst/add_NNFit"
 
 cd ${WORKDIR}
 
-for i in $(seq 0 9)
+if [ DRY_RUN -eq 0 ]; then
+    module load python/3.7
+fi
+
+for i in {1..29};
 do
-    ROOT_FILE="mc_mupage_end_${i}_cor_weighted.root"
+    ROOT_FILE="Antares_mupage_${i}_cor_weighted.root"
+    if [[ i -lt 10 ]]; then
+        H5_FILE="Mupage_000${i}"
+    else
+        H5_FILE="Mupage_00${i}"
+    fi
+
     COMMAND="python3 scripts/add_new_branches.py --r ${ROOT_FILE} --h5 ${H5_FILE} --task ${TASK} --particle muons"
 
-    echo " Executing command: "
-    echo -e "\n ========================================================="
+    echo -e "\n Executing command: "
+    echo -e " ========================================================="
     echo " ${COMMAND}"
     echo -e " =========================================================\n"
     eval "${COMMAND}"
